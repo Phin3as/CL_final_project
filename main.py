66d34610993ee74509f365961a1c0a8d44e992fd
Phin3as <marwaha.sajal@gmail.com>
2015-11-26 12:20:26 -0500

    Second Commit, pycharm settings and load_data function in tmp_util.py
    Started using github for windows, uploading all pycharm files, load data
    tested an commented.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-__author__ = 'Sajal'
+import time
+import tmp_util
+
+__author__ = 'Sajal/Harshal'
 
 # TODO: refactor name of file
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    train_X,train_Y,test_X=tmp_util.load_data()
+
+    end_time = time.time()
+    print("--- %s seconds ---" % (time.time() - start_time))
--- tmp_util.py
+++ tmp_util.py
@@ -1,3 +1,30 @@
 __author__ = 'Sajal/Harshal'
 
 # TODO : refactor name of file
+
+# name: load_data
+# params:
+# return: train_X,train_Y,test_X
+# The directory location is hard coded. It should be in the same place as this file in a folder called data.
+def load_data():
+    # SAJAL
+    input_dir = 'data/'
+    train_file = 'project_articles_train'
+    test_file = 'project_articles_test'
+
+    train_X=[]
+    train_Y=[]
+    test_X=[]
+
+    with open(input_dir+train_file,'r') as fp_train:
+        for line in fp_train:
+            data = line.strip().rsplit('\t',1)
+            train_X.append(data[0])
+            train_Y.append(data[1])
+
+    with open(input_dir+test_file,'r') as fp_test:
+        for ine in fp_test:
+            data = line.strip()
+            test_X.append(data)
+
+    return train_X,train_Y,test_X
